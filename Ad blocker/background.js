ca29022533cf0cf17d2c82a44a2bb3f0439cc04f
Anvesha Jain <anvesha19jain@gmail.com>
2022-03-07 10:21:05 +0530

    Added code for AdBlocker Extension

---
+++ Ad blocker/background.js
@@ -0,0 +1,19 @@
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
+
