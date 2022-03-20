4d3e3de1f57a8f604030a1066f49c9a11216d734
nik <77881638+nik132-eng@users.noreply.github.com>
2022-03-20 17:40:02 +0530

    Revert "applied changes & put all file and one folder"

--- Motivation Unlimited/script.js
+++
@@ -1,6 +0,0 @@
-      display_image();
-
-      function display_image(){
-        document.getElementById("image").src = "https://source.unsplash.com/300x300/?motivation,quote,success";
-      }
-      
