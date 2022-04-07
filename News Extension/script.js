353d40b00fcb1c661b291aaeb651faeb07a8a01d
Shreya Ghosh <72400676+shreya024@users.noreply.github.com>
2022-04-07 10:56:42 +0530

    Country choice implemented

--- News Extension/script.js
+++ News Extension/script.js
@@ -10,22 +10,24 @@ fetch(
 
     let createTaskCard = (task) => {
       let card = document.createElement("div");
-      card.className = "card";
+      card.className = "blog_card";
       card.style.width = "450px";
 
+      let cardMeta = document.createElement("div");
+      cardMeta.className = "meta";
+
       let cardImage = document.createElement("img");
-      cardImage.className = "card-img-top";
-      cardImage.style.width = "400px";
-      cardImage.style.height = "100px";
+      cardImage.className = "photo";
+      cardImage.style.width = "300px";
+      cardImage.style.height = "300px";
       cardImage.style.margin = "auto";
       cardImage.src = task.image_url;
       cardImage.alt = task.source_id;
 
       let cardBody = document.createElement("div");
-      cardBody.className = "card-body";
+      cardBody.className = "description";
 
-      let title = document.createElement("b");
-      title.className = "card-header";
+      let title = document.createElement("h5");
       title.innerText = task.title;
 
       let state = document.createElement("h6");
@@ -40,7 +42,7 @@ fetch(
       description.className = "card-text";
 
       let url = document.createElement("a");
-      url.className = "btn btn-primary";
+      url.className = ".read-more";
       url.innerText = "Read more";
       url.href = task.link;
       url.target = "_blank";
@@ -49,7 +51,8 @@ fetch(
       cardBody.appendChild(description);
       cardBody.appendChild(state);
       cardBody.appendChild(url);
-      card.appendChild(cardImage);
+      cardMeta.appendChild(cardImage);
+      card.appendChild(cardMeta);
       card.appendChild(cardBody);
       cardContainer.appendChild(card);
     };
@@ -84,22 +87,24 @@ function show() {
 
       let createTaskCard = (task) => {
         let card = document.createElement("div");
-        card.className = "card";
+        card.className = "blog_card";
         card.style.width = "450px";
 
+        let cardMeta = document.createElement("div");
+        cardMeta.className = "meta";
+
         let cardImage = document.createElement("img");
-        cardImage.className = "card-img-top";
-        cardImage.style.width = "400px";
-        cardImage.style.height = "100px";
+        cardImage.className = "photo";
+        cardImage.style.width = "300px";
+        cardImage.style.height = "300px";
         cardImage.style.margin = "auto";
         cardImage.src = task.image_url;
         cardImage.alt = task.source_id;
 
         let cardBody = document.createElement("div");
-        cardBody.className = "card-body";
+        cardBody.className = "description";
 
-        let title = document.createElement("b");
-        title.className = "card-header";
+        let title = document.createElement("h5");
         title.innerText = task.title;
 
         let state = document.createElement("h6");
@@ -114,7 +119,7 @@ function show() {
         description.className = "card-text";
 
         let url = document.createElement("a");
-        url.className = "btn btn-primary";
+        url.className = ".read-more";
         url.innerText = "Read more";
         url.href = task.link;
         url.target = "_blank";
@@ -123,7 +128,8 @@ function show() {
         cardBody.appendChild(description);
         cardBody.appendChild(state);
         cardBody.appendChild(url);
-        card.appendChild(cardImage);
+        cardMeta.appendChild(cardImage);
+        card.appendChild(cardMeta);
         card.appendChild(cardBody);
         cardContainer.appendChild(card);
       };
@@ -141,3 +147,82 @@ function show() {
     });
 }
 e.onchange = show;
+
+var ele = document.getElementById("country");
+function showEle() {
+  var cat = ele.options[ele.selectedIndex].text.substring(0, 2);
+  console.log(cat);
+  let country = cat.toLowerCase();
+  console.log(country);
+  fetch(
+    `https://newsdata.io/api/1/news?apikey=pub_6258cad616bb55ee634704d44f2a476b74a7&country=${country}`
+  )
+    .then((data) => data.json())
+    .then((issuesData) => {
+      console.log(issuesData);
+
+      const items = issuesData.results;
+      let cardContainer;
+
+      let createTaskCard = (task) => {
+        let card = document.createElement("div");
+        card.className = "blog_card";
+        card.style.width = "450px";
+
+        let cardMeta = document.createElement("div");
+        cardMeta.className = "meta";
+
+        let cardImage = document.createElement("img");
+        cardImage.className = "photo";
+        cardImage.style.width = "300px";
+        cardImage.style.height = "300px";
+        cardImage.style.margin = "auto";
+        cardImage.src = task.image_url;
+        cardImage.alt = task.source_id;
+
+        let cardBody = document.createElement("div");
+        cardBody.className = "description";
+
+        let title = document.createElement("h5");
+        title.innerText = task.title;
+
+        let state = document.createElement("h6");
+        state.innerText = task.pubDate;
+
+        let description = document.createElement("p");
+        let string = task.description;
+        if (string.length > 150) {
+          string = string.substring(0, 149) + "...";
+        }
+        description.innerText = string;
+        description.className = "card-text";
+
+        let url = document.createElement("a");
+        url.className = ".read-more";
+        url.innerText = "Read more";
+        url.href = task.link;
+        url.target = "_blank";
+
+        cardBody.appendChild(title);
+        cardBody.appendChild(description);
+        cardBody.appendChild(state);
+        cardBody.appendChild(url);
+        cardMeta.appendChild(cardImage);
+        card.appendChild(cardMeta);
+        card.appendChild(cardBody);
+        cardContainer.appendChild(card);
+      };
+
+      const newArray = items.slice(0, 5);
+      let initListOfTasks = () => {
+        document.getElementById("issues").innerHTML = "";
+        cardContainer = document.getElementById("issues");
+        newArray.forEach((task) => {
+          createTaskCard(task);
+        });
+      };
+
+      initListOfTasks();
+    });
+}
+ele.onchange = showEle;
