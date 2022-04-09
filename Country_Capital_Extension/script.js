c199e0df977411cea629ec275ed746fcc470cfd7
Sukriti-m <sukritimaurya.fzd@gmail.com>
2022-04-09 11:16:32 +0530

    add functionality

--- Country_Capital_Extension/script.js
+++ Country_Capital_Extension/script.js
@@ -1,32 +1,18 @@
-/*async function fetchData(){const country=document.frm.country.value;
-try{const res=await fetch("https://restcountries.com/v3.1/name/"+ country );
-const record=await res.json();
-document.getElementById("capital").innerHTML=record.capital;}
-catch (err) {
-    console.error(err);
-  }
+function seeCap(country){
+  fetch("https://restcountries.com/v3.1/name/"+country)
+  .then(data => data.json())
+  .then(capitalData => {
+      const capitalText = capitalData[0].capital;
+      const capitals = document.querySelector(".capitals");
+      
+      capitals.textContent = capitalText;
+  })
 }
-document.getElementById("btn").onclick=fetchData();
-*/
-function yu(){let clk=country.value;
-  console.log(clk);
-   capital.textContent =clk;}
-//const form = document.querySelector('form');
-const btn = document.querySelector(".btn");
-const country = document.querySelector(".country");
-const capital = document.querySelector(".capital");
-btn.addEventListener("click", yu);
-  /*  fetch("https://api.kanye.rest/")
-    .then(data => data.json())
-    .then(capitalData => {
-        const capitalText = capitalData.quote;
 
-        capital.textContent = capitalText;
-    });*/
+document.getElementById("clickme").addEventListener("click",()=>{
+  const country=document.querySelector(".country").value;
+  console.log(country);
+seeCap(country);});
 
-   // results.textContent = "Please enter a valid number!";
-  //  let totalInches = fetch("https://restcountries.com/v3.1/name/"+ feet );
-    //let totalInchess=totalInches.json();
-   // capital.textContent = `${api[0].capital}`;
-    //document.querySelector('#country').value = '';
-//})
+
+  
