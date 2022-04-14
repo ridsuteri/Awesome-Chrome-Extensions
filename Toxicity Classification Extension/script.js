5f31b7636d39e9385bea95782c11fbdee5eb916f
Shreya Ghosh <72400676+shreya024@users.noreply.github.com>
2022-04-14 07:10:53 +0530

    script.js updated

--- Toxicity Classification Extension/script.js
+++ Toxicity Classification Extension/script.js
@@ -1,6 +1,3 @@
-// The minimum prediction confidence.
-const threshold = 0.9;
-
 const inputText = document.getElementById("sent");
 
 const enterText = document.getElementById("enterText");
@@ -10,29 +7,49 @@ const result = document.getElementById("result");
 inputText.onchange = function () {
   console.log(inputText.value);
   let sent = inputText.value;
+  result.innerHTML = "Toxicity Results Loading...";
   // Load the model. Users optionally pass in a threshold and an array of
   // labels to include.
-  toxicity.load(threshold).then((model) => {
-    const sentences = sent;
-
-    model.classify(sentences).then((predictions) => {
-      // `predictions` is an array of objects, one for each prediction head,
-      // that contains the raw probabilities for each input along with the
-      // final prediction in `match` (either `true` or `false`).
-      // If neither prediction exceeds the threshold, `match` is `null`.
-
-      console.log(predictions);
-      result.innerHTML = "";
-      for (let i = 0; i < predictions.length; i++) {
-        if (predictions[i].results[0].match) {
-          result.innerHTML += predictions[i].label + " ";
-        }
-      }
-      if (result.innerHTML == "") {
-        result.innerHTML = "This text is not toxic";
-      } else {
-        result.innerHTML += " ---labels of Toxicity detected";
-      }
+  let _data = {
+    comment: {
+      text: sent,
+    },
+    languages: ["en"],
+    requestedAttributes: {
+      TOXICITY: {},
+    },
+  };
+  fetch(
+    "https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=AIzaSyAOvxdB7IKKA0TphPvY2sFMlHDWBVLlV9o",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(_data),
+    }
+  )
+    .then((data) => data.json())
+    .then((commentData) => {
+      console.log(
+        commentData.attributeScores.TOXICITY.spanScores[0].score.value
+      );
+      let score =
+        commentData.attributeScores.TOXICITY.spanScores[0].score.value;
+      let toxicity;
+      if (score == 1) toxicity = "insult";
+      else if (score < 1 && score >= 0.99) toxicity = "rejection / toxic";
+      else if (score < 0.99 && score >= 0.73) toxicity = "profanity";
+      else if (score < 0.93 && score >= 0.75) toxicity = "severe toxicity";
+      else if (score < 0.75 && score >= 0.15) toxicity = "threat";
+      else if (score < 0.15 && score >= 0.04) toxicity = "sexually explicit";
+      else if (score < 0.04 && score >= 0.03) toxicity = "identity attack";
+      else toxicity = "not toxic";
+      result.innerHTML =
+        "This comment is classified to be " +
+        toxicity +
+        " with " +
+        score * 100 +
+        " % of toxicity";
     });
-  });
 };
