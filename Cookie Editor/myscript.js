fe7175e6d61c8bc6144a233b5dae8ac4b9ff4b2d
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-05 21:33:40 -0700

    :memo: file formated

--- Cookie Editor/myscript.js
+++ Cookie Editor/myscript.js
@@ -1,46 +1,46 @@
-document.getElementById('id_button_getAll').onclick = () => {
-    chrome.cookies.getAll(getDetails('getAll'), ((cookies) => {
-        let text = 'Number of cookies=' + cookies.length + '\n';
-        for (let cookie of cookies) {
-            text += 'domain=' + cookie.domain + ',path=' + cookie.path + ',name=' + cookie.name + ',value=' + cookie.value + '\n';
-        }
-        document.getElementById('id_text').value = text;
-    }));
+﻿document.getElementById('id_button_getAll').onclick = () => {
+  chrome.cookies.getAll(getDetails('getAll'), ((cookies) => {
+    let text ='Number of cookies=' + cookies.length + '\n';
+    for (let cookie of cookies){
+      text += 'domain=' + cookie.domain + ',path=' + cookie.path + ',name=' + cookie.name + ',value=' + cookie.value + '\n';
+    }
+    document.getElementById('id_text').value = text;
+  }));
 }
 
 document.getElementById('id_button_set').onclick = () => {
-    chrome.cookies.set(getDetails('set'));
+  chrome.cookies.set(getDetails('set'));
 }
 
 document.getElementById('id_button_remove').onclick = () => {
-    chrome.cookies.remove(getDetails('remove'));
+  chrome.cookies.remove(getDetails('remove'));
 }
 
-function getDetails(kind) {
-    let domain = document.getElementById('id_domain').value;
-    let path = document.getElementById('id_path').value;
-    let name = document.getElementById('id_name').value;
-    let value = document.getElementById('id_value').value;
-    let details = {};
+function getDetails(kind){
+  let domain =document.getElementById('id_domain').value;
+  let path = document.getElementById('id_path').value;
+  let name = document.getElementById('id_name').value;
+  let value = document.getElementById('id_value').value;
+  let details = {};
 
-    switch (kind) {
-        case 'getAll':
-            if (domain != '') {
-                details['domain'] = domain;
-            }
-            if (name != '') {
-                details['name'] = name;
-            }
-            break;
-        case 'set':
-            details['url'] = 'https://' + domain + path;
-            details['name'] = name;
-            details['value'] = value;
-            break;
-        case 'remove':
-            details['url'] = 'https://' + domain + path;
-            details['name'] = name;
-            break;
+  switch(kind){
+  case 'getAll':
+    if (domain != ''){
+      details['domain'] = domain;
     }
-    return details;
+    if (name != ''){
+      details['name'] = name;
+    }
+    break;
+  case 'set':
+    details['url'] = 'https://' + domain + path;
+    details['name'] = name;
+    details['value'] = value;
+    break;
+  case 'remove':
+    details['url'] = 'https://' + domain + path;
+    details['name'] = name;
+    break;
+  }
+  return details;
 }
