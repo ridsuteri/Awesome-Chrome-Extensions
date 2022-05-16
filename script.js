a8aa55070700c3e6d6d5043961460ba3a2a3b7ca
Praniti <pranitip111@gmail.com>
2022-05-16 11:32:32 +0530

    Website changes made

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let projectData = [
   {
-    projectName: "Ad Blocker",
-
+    projectName: "Ad-Blocker",
+    projectImage: "Ad-Blocker/AdBlockerimage.png",
     projectUrl: "",
   },
   {
-    projectName: "Auto Refresher",
-
+    projectName: "Auto-Refresher",
+    projectImage: "Auto-Refresher/images/example-go.png",
+    projectUrl: "Auto-Refresher/popup.html",
   },
   {
     projectName: "Binary_To_Decimal_converter",
@@ -40,6 +41,8 @@ let projectData = [
   },
   {
     projectName: "List The Bookmarks",
+    projectImage: "List-The-Bookmarks/List-the-Bookmarks-Images.png",
+    projectUrl: "List-The-Bookmarks/popup.html",
 
   },
   {
@@ -49,12 +52,14 @@ let projectData = [
   },
   {
     projectName: "Motivation_unlimited",
-
+    projectImage: "Motivation_unlimited/Motivation-Unlimited-Image.png",
     projectUrl: "Motivation_unlimited/popup.html",
   },
 
   {
     projectName: "News Extension",
+    projectImage: "News-Extension/images/img0.JPG",
+    projectUrl: "News-Extension/popup.html",
 
   },
   {
@@ -64,7 +69,7 @@ let projectData = [
   },
   {
     projectName: "Pinterest_extension",
-
+    projectImage: "Pinterest_extension/Pinterest-Image.jpg",
     projectUrl: "Pinterest_extension/popup.html",
   },
   {
@@ -79,6 +84,8 @@ let projectData = [
   },
   {
     projectName: "Puzzle Game",
+    projectImage: "Puzzle-Game/img/Screenshot-2022-04-08 030523.png",
+    projectUrl: "Puzzle-Game/index.html",
 
   },
   {
@@ -88,15 +95,19 @@ let projectData = [
   },
   {
     projectName: "Stop Watch",
-    projectUrl: "TIC-TAC-TOE/double.html",
+    projectImage: "Stop-Watch/Stop-Watch-Image.png",
+    projectUrl: "Stop-Watch/popup.html",
   },
   {
     projectName: "Time Zone Finder",
-
+    projectImage: "Time-Zone-Finder/TimeZone-Finder-Image.png",
+    projectUrl: "Time-Zone-Finder/index.html",
   },
 
   {
     projectName: "Toxicity Classification Extension",
+    projectImage: "Toxicity-Classification-Extension/images/img0.JPG",
+    projectUrl: "Toxicity-Classification-Extension/popup.html",
 
   },
   {
@@ -106,8 +117,8 @@ let projectData = [
   },
   {
     projectName: "Weather app",
-
-    projectUrl: "calculator/popup.html",
+    projectImage: "Weather-app/images/icon.png",
+    projectUrl: "Weather-app/popup.html",
   },
   {
     projectName: "currency-calc-extension",
@@ -127,88 +138,250 @@ let projectData = [
 
   {
     projectName: "rgb to hex convertor",
+    projectImage: "rgb-to-hex-convertor/image/2.png",
+    projectUrl: "rgb-to-hex-convertor/popup.html",
 
   },
   {
     projectName: "Auto Right",
-    projectImage: "",
+    projectImage: "Auto Right/Auto right image.png",
     projectUrl: "Auto Right/index.html",
   },
   {
     projectName: "Background Remover",
-    projectImage: "",
+    projectImage: "Background Remover/logo.png",
     projectUrl: "Background Remover/popup.html",
   },
   {
     projectName: "Books_Recommendation",
-    projectImage: "",
+    projectImage: "Books_Recommendation/book.jpg",
     projectUrl: "Books_Recommendation/popup.html",
   },
   {
     projectName: "Calculate Age",
-    projectImage: "",
+    projectImage: "Calculate Age/logo.png",
     projectUrl: "Calculate Age/index.html",
   },
   {
     projectName: "Company Insider",
-    projectImage: "",
+    projectImage: "Company Insider/images/img1.JPG",
     projectUrl: "Company Insider/popup.html",
   },
   {
     projectName: "Control Chrome Tabs",
-    projectImage: "",
+    projectImage: "Control Chrome Tabs/img/000001.png",
     projectUrl: "",
   },
   {
     projectName: "Food_Calorie_Extension",
-    projectImage: "",
+    projectImage: "Food_Calorie_Extension/bg.jpg",
     projectUrl: "Food_Calorie_Extension/popup.html",
   },
   {
     projectName: "Movies_Recommendation",
-    projectImage: "",
+    projectImage: "Movies_Recommendation/movie.jpg",
     projectUrl: "Movies_Recommendation/popup.html",
   },
   {
     projectName: "Music player",
-    projectImage: "",
+    projectImage: "Music player/musician-349790.jpg",
     projectUrl: "Music player/index.html",
   },
   {
     projectName: "Note Pad",
-    projectImage: "",
+    projectImage: "Note Pad/img/Screenshot 2022-04-22 113100.png",
     projectUrl: "Note Pad/popup.html",
   },
   {
     projectName: "Phone Checker",
-    projectImage: "",
+    projectImage: "Phone Checker/images/img0.JPG",
     projectUrl: "Phone Checker/popup.html",
   },
   {
     projectName: "Quick Access Google",
-    projectImage: "",
+    projectImage: "Quick Access Google/img/Screenshot 2022-04-12 071532.png",
     projectUrl: "Quick Access Google/popup.html",
   },
   {
     projectName: "Save_environment",
-    projectImage: "",
+    projectImage: "Save_environment/bg.jpg",
     projectUrl: "Save_environment/popup.html",
   },
   {
     projectName: "Twitter Context Menu",
-    projectImage: "",
+    projectImage: "Twitter Context Menu/img/aok2.png",
     projectUrl: "",
   },
   {
     projectName: "User taking form",
-    projectImage: "",
+    projectImage: "User taking form/User Taking Form image.png",
     projectUrl: "User taking form/index.html",
   },
   {
     projectName: "VAT_Calculator",
-    projectImage: "",
+    projectImage: "VAT_Calculator/images/workingImage.jpg",
     projectUrl: "VAT_Calculator/popup.html",
-  }
+  },
+  {
+    projectName: "Auto Fill",
+    projectImage: "Auto Fill/autofill image.png",
+    projectUrl: "Auto Fill/index.html",
+  },
+  {
+    projectName: "BMI-Calculator",
+    projectImage: "BMI-Calculator/BMIExtentionWorkingImages/BMIextention.png",
+    projectUrl: "BMI-Calculator/popup.html",
+  },
+  {
+    projectName: "Calculator",
+    projectImage: "calculator/Calculator-Image.png",
+    projectUrl: "calculator/popup.html",
+  },
+  {
+    projectName: "Calender-2",
+    projectImage: "Calender-2/Dynamic-Calender-images/Dynamic Calender2.png",
+    projectUrl: "Calender-2/index.html",
+  },
+  {
+    projectName: "Code-Launcher",
+    projectImage: "Code-Launcher/images/Code-Aider-image.png",
+    projectUrl: "Code-Launcher/popup.html",
+  },
+  {
+    projectName: "Coin-toss",
+    projectImage: "Coin-toss/Coin-Toss-Images.png",
+    projectUrl: "Coin-toss/index.html",
+  },
+  {
+    projectName: "Color Picker",
+    projectImage: "Color Picker/color picker image.png",
+    projectUrl: "Color Picker/index.html",
+  },
+  {
+    projectName: "Competitive-Trainer",
+    projectImage: "Competitive-Trainer/Competitive-Trainer-Image.png",
+    projectUrl: "Competitive-Trainer/html/leetcode.html",
+  },
+  {
+    projectName: "Cookie Editor",
+    projectImage: "Cookie Editor/img/sscookie.png",
+    projectUrl: "Cookie Editor/popup.html",
+  },
+  {
+    projectName: "Countdown-timer",
+    projectImage: "Countdown-timer/CountDown-Timer-Image.png",
+    projectUrl: "Countdown-timer/index.html",
+  },
+  {
+    projectName: "Decimal_Hexadecimal_Converter",
+    projectImage: "Decimal_Hexadecimal_Converter/Decimal Hexadecimal Convertor image.png",
+    projectUrl: "Decimal_Hexadecimal_Converter/popup.html",
+  },
+  {
+    projectName: "Facts_Extension",
+    projectImage: "Facts_Extension/bg.jpg",
+    projectUrl: "Facts_Extension/popup.html",
+  },
+  {
+    projectName: "File Counter",
+    projectImage: "File counter/img/ssfilecount.png",
+    projectUrl: "File counter/popup.html",
+  },
+  {
+    projectName: "GK_Quiz",
+    projectImage: "GK_Quiz/bg.png",
+    projectUrl: "GK_Quiz/popup.html",
+  },
+  {
+    projectName: "Guess_Cartoon",
+    projectImage: "Guess_Cartoon/Guess cartoon image.png",
+    projectUrl: "Guess_Cartoon/popup.html",
+  },
+  {
+    projectName: "Height-Conversion-tool",
+    projectImage: "Height-Conversion-tool/Height-Conversion-Tool-Image.png",
+    projectUrl: "Height-Conversion-tool/index.html",
+  },
+  {
+    projectName: "IT_Income_Tax_Calculator",
+    projectImage: "IT_Income_Tax_Calculator/IT-IncomeTax-cal-Image.png",
+    projectUrl: "IT_Income_Tax_Calculator/it.html",
+  },
+  {
+    projectName: "Links-Launcher",
+    projectImage: "Links-Launcher/Links-Launcher-Image.png",
+    projectUrl: "Links-Launcher/popup.html",
+  },
+  {
+    projectName: "Loan-Calculator",
+    projectImage: "Loan-calculator/Loan-Cal-Image.png",
+    projectUrl: "Loan-calculator/index.html",
+  },
+  {
+    projectName: "Markdown Preview",
+    projectImage: "Markdown Preview/markdown preview image.png",
+    projectUrl: "Markdown Preview/index.html",
+  },
+  {
+    projectName: "Recipe Extension",
+    projectImage: "Markdown Preview/markdown preview image.png",
+    projectUrl: "Recipe Extension/images/img0.JPG",
+  },
+  {
+    projectName: "Riddle_Extension",
+    projectImage: "Riddle_Extension/bg.png",
+    projectUrl: "Riddle_Extension/popup.html",
+  },
+  {
+    projectName: "Smartphone Call",
+    projectImage: "Smartphone Call/1.jpg",
+    projectUrl: "Smartphone Call/index.html",
+  },
+  {
+    projectName: "Speech Recognition",
+    projectImage: "Speech Recognition/img/ss2speech.png",
+    projectUrl: "Speech Recognition/index.html",
+  },
+  {
+    projectName: "Text To Speech",
+    projectImage: "Text To Speech/preview1.jpg",
+    projectUrl: "Text To Speech/index.html",
+  },
+  {
+    projectName: "TIC-TAC-TOE",
+    projectImage: "TIC-TAC-TOE/TICTACTOE-Image.png",
+    projectUrl: "TIC-TAC-TOE/double.html",
+  },
+  {
+    projectName: "To-Do-Extension",
+    projectImage: "To-Do-Extension/images/workingimage.png",
+    projectUrl: "To-Do-Extension/popup.html",
+  },
+  {
+    projectName: "Translator",
+    projectImage: "Translator/img/Screenshot 2022-04-29 222446.png",
+    projectUrl: "Translator/index.html",
+  },
+  {
+    projectName: "Vocabulary Extension",
+    projectImage: "Vocabulary Extension/bg.jpg",
+    projectUrl: "Vocabulary Extension/popup.html",
+  },
+  {
+    projectName: "Weight conversion tool",
+    projectImage: "Weight conversion tool/Weight Convertor Image.png",
+    projectUrl: "Weight conversion tool/index.html",
+  },
+  {
+    projectName: "Wiki Search",
+    projectImage: "Wiki Search/wiki search image.png",
+    projectUrl: "",
+  },
+  {
+    projectName: "Window_Remover",
+    projectImage: "Window_Remover/img/winR.png",
+    projectUrl: "Window_Remover/popup.html",
+  },
 ];
 
 var projectDetails = projectData.slice(0);
