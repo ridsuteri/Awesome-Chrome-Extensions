0569b1bf45fb76a0d9af49cdc4e56294d3481d09
Ankit Rao <72241207+DragonUncaged@users.noreply.github.com>
2022-05-31 17:31:09 +0530

    File Size Viewer (#435)
    * File Size Viewer
    
    * Update readme.md

---
+++ File Size Viewer and Downloader Github/background.js
@@ -0,0 +1,30 @@
+const MessageType = {
+  PAGE_RENDERED: 'pageRendered'
+};
+
+let currentUrl = '';
+let tabId;
+chrome.webRequest.onCompleted.addListener(
+  function(details) {
+    const parsedUrl = new URL(details.url);
+
+    if (currentUrl && currentUrl.indexOf(parsedUrl.pathname) > -1 && tabId) {
+      chrome.tabs.sendMessage(tabId, { type: MessageType.PAGE_RENDERED });
+    }
+  },
+  { urls: ['*://*.github.com/*'] }
+);
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+  details => {
+    tabId = details.tabId;
+    currentUrl = details.url;
+  },
+  {
+    url: [
+      {
+        hostSuffix: 'github.com'
+      }
+    ]
+  }
+);
