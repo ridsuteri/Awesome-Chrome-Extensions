109636735ba858b9a0adce7cc8fc078717fb4499
newbie2508 <97738900+newbie2508@users.noreply.github.com>
2022-09-29 21:43:02 +0530

    Update app.js

--- Height-Conversion-tool/app.js
+++ Height-Conversion-tool/app.js
@@ -1,15 +1,18 @@
-const form = document.querySelector('form');
-
-form.addEventListener('submit', function (e) {
-    //grab content from feet input
-    let feet = document.querySelector('#feet');
-    const results = document.querySelector('#results');
-    e.preventDefault();
-
-    feet = parseFloat(feet.value);
-
-    results.textContent = "Please enter a valid number!";
-    let totalInches = (feet * 30.48);
-    results.textContent = `${totalInches} cm`;
-    document.querySelector('#feet').value = '';
-})
+document.getElementById("clickme").addEventListener("click",()=>{    
+	const a=document.querySelector(".feet").value;
+	const b=document.querySelector(".inches").value;
+	const output=document.querySelector(".output");
+	 const d=a*30.48+b*2.54;
+	 console.log(d);
+	 output.textContent="Converted Height is "+ d+"cm";
+	document.querySelector(".output").style.color= "white";
+	});
+  
+	document.getElementById("reset").addEventListener("click",()=>{  
+	let a=document.querySelector(".feet");
+	let b=document.querySelector(".inches");
+	let output=document.querySelector(".output");
+	  a.value="";
+	  b.value="";
+	output.textContent= " ";
+  });
